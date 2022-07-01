cc87e6ab11af26064a12edf6bbeec92dea74611f
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-07-01 10:28:14 +0530

    DataTypes

---
+++ 2.DataTypes/main.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    /*#ifndef ONLINE_JUDGE
+        freopen("input.txt","r", stdin);
+        freopen("output.txt","w",stdout);
+    #endif*/
+
+    int i;
+    cout<<"size of integer "<<sizeof(i)<<"\n";
+
+    float f;
+    cout<<"size of float "<<sizeof(f)<<endl;
+
+    char c;
+    cout<<"size of character "<<sizeof(c)<<endl;
+
+    bool b;
+    cout<<"size of boolean "<<sizeof(b)<<endl;
+
+    short int si;
+    cout<<"size of short int "<<sizeof(si)<<endl;
+
+    long int li;
+    cout<<"size of long int "<<sizeof(li)<<endl;
+
+    return 0;  
+}
