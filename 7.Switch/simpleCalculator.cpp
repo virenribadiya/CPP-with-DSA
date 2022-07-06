7aa316429b2326f642c463639779479063bc32a6
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-07-06 14:44:27 +0530

    simpleCalculator Using Switch Cases

---
+++ 7.Switch/simpleCalculator.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float a,b;
+    cin>>a>>b;
+    char op;
+    cout<<"Enter an operator:";
+    cin>>op;
+    switch (op)
+    {
+    case '+':
+        cout<<a+b<<endl;
+        break;
+    
+    case '-':
+        cout<<a-b<<endl;
+        break;
+    
+    case '*':
+        cout<<a*b<<endl;
+        break;
+    
+    case '/':
+        cout<<a/b<<endl;
+        break;
+    default:
+        cout<<"please enter valid operator";
+        break;
+    }
+    return 0;
+}
