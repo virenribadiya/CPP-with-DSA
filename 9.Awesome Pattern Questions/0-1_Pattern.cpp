226b964c00f078467a4f9cb21cfec144fd7ec516
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-07-08 18:55:41 +0530

    Awesome Pattern Questions

---
+++ 9.Awesome Pattern Questions/0-1_Pattern.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int value=1;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<value<<" ";
+            if(value==1)
+            {
+                value=0;
+            }
+            else
+            {
+                value=1;
+            }
+        }
+        cout<<endl;
+    }
+}
