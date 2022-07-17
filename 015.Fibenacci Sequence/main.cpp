6d194601749c5dd4fadd73574dcb7f9556bc9f9f
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-07-17 17:09:28 +0530

    Fibenacci Sequence added

---
+++ 015.Fibenacci Sequence/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void fib(int num)
+{
+    int term1=0;
+    int term2=1;
+    for(int i=1;i<=num;i++)
+    {
+        cout<<term1<<" ";
+        int temp=term1+term2;
+        term1=term2;
+        term2=temp;
+    }
+}
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    fib(n);
+    return 0;
+}
