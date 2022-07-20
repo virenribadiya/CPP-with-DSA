b7a90934c6aeb06aeb841b108493890aa121d627
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-07-20 17:42:59 +0530

    Pascal Triangle added

---
+++ 018.Pascal Triangle/main.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int fact(int num)
+{
+    int factorial=1;
+    for(int i=2;i<=num;i++)
+    {
+        factorial*=i;
+    }
+    return factorial;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            cout<<fact(i)/(fact(i-j)*fact(j))<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
