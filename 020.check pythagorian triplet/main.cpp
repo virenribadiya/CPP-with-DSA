525ffd1d4ca8fb1af8063549d222766dd2449642
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-07-22 16:07:27 +0530

    pythagorian triplet

---
+++ 020.check pythagorian triplet/main.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+bool check(int x,int y,int z)
+{
+    int a,b,c;
+    a=max(x,max(y,z));
+    if(a==x)
+    {
+        b=y;
+        c=z;
+    }
+    else if(a==y)
+    {
+        b=x;
+        c=z;
+    }
+    else
+    {
+        b=x;
+        c=y;
+    }
+
+    if(a*a==b*b+c*c)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    int x,y,z;
+    cin>>x>>y>>z;
+
+    if(check(x,y,z))
+    {
+        cout<<"Pythagorian Triplate"<<endl;
+    }
+    else
+    {
+        cout<<"not a Pythagorian triplet"<<endl;
+    }
+    return 0;
+}
