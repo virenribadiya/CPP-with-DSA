3dc7c00e3e00d02e9fb9b73e0335e20d3bba35a0
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-07-23 10:13:49 +0530

    Binary to decimal conversion program added

---
+++ 21.Binary to Decimal/Binary_to_Decimal.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int binaryToDecimal(int n)
+{
+    int ans=0;
+    int poweranswer=1;
+    while (n>0)
+    {
+       int lastdigit=n%10;
+       ans=ans+(lastdigit*poweranswer);
+       poweranswer=poweranswer*2;
+       n=n/10;
+    }
+    return ans;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<binaryToDecimal(n)<<endl;
+    return 0;
+}
