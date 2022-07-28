f0ea9be46b8282fbc07b848bb32d023d42b9c1e8
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-07-28 20:14:48 +0530

    Update main.cpp

--- 012.Armstrong number or not/main.cpp
+++ 012.Armstrong number or not/main.cpp
@@ -1,5 +1,8 @@
 //program to check armstrong number.
 
+// An Armstrong number of three digits is an integer such that the sum of the cubes of its digits is equal to the number itself. 
+// For example, 371 is an Armstrong number since 3**3 + 7**3 + 1**3 = 371.
+
 #include<iostream>
 #include<cmath>
 using namespace std;
