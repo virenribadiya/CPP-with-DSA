e272c37acd66e26d239a826dedd9d6381c29d743
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-07-29 18:58:40 +0530

    Update main.cpp

--- 015.Fibenacci Sequence/main.cpp
+++ 015.Fibenacci Sequence/main.cpp
@@ -8,7 +8,7 @@ void fib(int num)
 {
     int term1=0;
     int term2=1;
-    for(int i=1;i<=num;i++) 
+    for(int i=1;i<=num;i++) // logic for printing fibonacci series
     {
         cout<<term1<<" ";
         int temp=term1+term2;
