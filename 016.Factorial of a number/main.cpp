d3738f43037b69da6045eabb997975ad8f9c4fbe
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-07-29 18:59:47 +0530

    Update main.cpp

--- 016.Factorial of a number/main.cpp
+++ 016.Factorial of a number/main.cpp
@@ -1,10 +1,13 @@
+// Program to calculate factorial of a number.
+
 #include<iostream>
 using namespace std;
 
-int fact(int num)
+int fact(int num) 
 {
+    //logic
     int factorial=1;
-    for(int i=2;i<=num;i++)
+    for(int i=2;i<=num;i++) 
     {
         factorial*=i;
     }
