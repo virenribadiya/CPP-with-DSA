d2d0958d013240705e0fc004a1adb745955f1704
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-08-01 18:39:12 +0530

    Add files via upload

---
+++ 026.Binary Search in Arrays/binarySearch.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+int binarySearch(int array[],int n,int key)
+{
+    int s=0;
+    int e=n;
+    
+    // Time complexity = O(log(n))
+    while(s<=e)
+    {
+        int mid =(s+e)/2;
+        if(array[mid]==key)
+        {
+            return mid;
+        }
+        else if(array[mid]>key)
+        {
+            e=mid-1;
+        }
+        else
+        {
+            s=mid+1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+
+    int key;
+    cin>>key;
+
+    cout<<binarySearch(arr,n,key)<<endl;
+
+    return 0;
+}
