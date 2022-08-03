5b5f086373ccbd01b7ca9fe45fe88e6b21262de0
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-08-03 18:56:04 +0530

    Update main.cpp

--- 029.Sum of each Subarray of an Array/main.cpp
+++ 029.Sum of each Subarray of an Array/main.cpp
@@ -3,6 +3,13 @@
     - No. of subarrays of an array with n elemnts = nC2 + n.
 */
 
+/* Subsequence 
+    - sequence that can be derived an array by 
+      selecting zero or more elements, without
+      changing the order of the remaining elements.
+    - No. of subsequences of an array with n elements =2^n.  
+*/
+
 // Problem - sum of all subarrays of an array having n elements.
 
 #include<iostream>
