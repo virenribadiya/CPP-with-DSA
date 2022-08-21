22d36b92f4685ff23ff035683a733475d8bf98cf
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-08-21 16:18:55 +0530

    solution code added

---
+++ 030.Longest Arithmetic Subarray ( Google Kick Start )/solution.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int ans=2; // at initial stage longest subarray is first two elements and its length is 2.
+    int curr=2;
+    int pd=arr[1]-arr[0];
+    int j=2; // we are starting to compare from index 2 because we have assume that first two index are our arithmetic sub array.
+
+    while(j<n)
+    {
+        if(pd==arr[j]-arr[j-1])
+        {
+            curr++;
+        }
+        else
+        {
+            pd=arr[j]-arr[j-1];
+            curr=2;
+        }
+        ans=max(curr,ans);
+        j++;
+    }
+    cout<<ans<<endl;
+    return 0;
+}
