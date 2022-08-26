4c69e2fe9440e889ace720c9903f6a275b3beb03
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-08-26 21:17:26 +0530

    optimized code added

--- 036.Maximum subarray Sum/optimized.cpp
+++ 036.Maximum subarray Sum/optimized.cpp
@@ -1,1 +1,27 @@
+#include<iostream>
+#include<climits>
+using namespace std;
 
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+
+    int sum,maxSum=INT_MIN;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i;j<n;j++)
+        {
+            sum+=a[j];
+            maxSum=max(sum,maxSum);
+        }
+        sum=0;
+    }
+    cout<<maxSum<<endl;
+    return 0;
+}
