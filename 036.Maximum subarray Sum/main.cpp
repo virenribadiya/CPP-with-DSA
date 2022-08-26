87edd49d27f37f3faa2b39fa8c9bfef4ad7e304f
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-08-26 21:15:44 +0530

    Update main.cpp

--- 036.Maximum subarray Sum/main.cpp
+++ 036.Maximum subarray Sum/main.cpp
@@ -1,1 +1,32 @@
 
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    
+    int maxSum=INT_MIN;
+    int sum=0;
+    for(int i=0;i<n;i++)// to select starting point
+    {
+        for(int j=0;j<n;j++) // to select ending point
+        {
+            for(int k=i;k<=j;k++) // to find sum of an subarray.
+            {
+               sum+=a[k]; 
+            }
+            maxSum=max(maxSum,sum);
+            sum=0;
+        }
+    }
+    cout<<maxSum<<endl;
+    return 0;
+}
