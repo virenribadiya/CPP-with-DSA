42ee6755eea76f28a6148bcdcdc7da8b6621f72d
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-08-27 21:38:32 +0530

    Update KadanesAlgo.cpp

--- 037.Kadane's Algorithm/KadanesAlgo.cpp
+++ 037.Kadane's Algorithm/KadanesAlgo.cpp
@@ -1,1 +1,29 @@
 
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+
+    int maxSum=INT_MIN;
+    int currentSum=0;
+    for(int i=0;i<n;i++)
+    {
+        currentSum=a[i];
+        maxSum=max(maxSum,currentSum);
+        if(currentSum<0)
+        {
+            currentSum=0;
+        }
+    }
+    cout<<maxSum<<endl;
+    return 0;
+}
