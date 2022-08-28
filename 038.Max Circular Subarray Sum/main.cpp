621fd170dadffc2c1c3ca86c2fc89c2561cab20e
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-08-28 21:20:28 +0530

    Update main.cpp

--- 038.Max Circular Subarray Sum/main.cpp
+++ 038.Max Circular Subarray Sum/main.cpp
@@ -35,7 +35,7 @@ int main()
     for(int i=0;i<n;i++)
     {
         totalSum+=a[i];
-        a[i]=-a[i];
+        a[i]=-a[i]; // sign changing of the elements
     }
 
     wrapSum=totalSum+kadane(a,n);
