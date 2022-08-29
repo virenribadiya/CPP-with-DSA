ee22bb6c1b2435cf61bed281afcf84d414a5547b
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-08-29 14:17:39 +0530

    Update main.cpp

--- 039.PairSum Problem/main.cpp
+++ 039.PairSum Problem/main.cpp
@@ -1,1 +1,28 @@
 
+#include<iostream>
+using namespace std;
+
+bool pairSum(int a[],int n,int k)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[j]+a[i]==k)
+            {
+                cout<<i<<" "<<j<<endl;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    int a[]={2,4,7,11,14,16,20,21};
+    int k=31;
+
+    pairSum(a,8,k);
+    return 0;
+}
