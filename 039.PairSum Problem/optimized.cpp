8cd7e0fbc890a38d770bedab5a4d8a3e1a3b629e
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-08-29 14:18:07 +0530

    Update optimized.cpp

--- 039.PairSum Problem/optimized.cpp
+++ 039.PairSum Problem/optimized.cpp
@@ -1,1 +1,39 @@
 
+
+
+
+#include<iostream>
+using namespace std;
+
+bool pairSum(int a[],int n,int k)
+{
+    int high=n-1;
+    int low=0;
+
+    while(low<high)
+    {
+        if(a[low]+a[high]==k)
+        {
+            cout<<low<<" "<<high<<endl;
+            return true;
+        }
+        else if(a[low]+a[high]>k)
+        {
+            high--;
+        }
+        else
+        {
+            low++;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    int a[]={2,4,7,11,14,16,20,21};
+    int k=31;
+
+    pairSum(a,8,k);
+    return 0;
+}
