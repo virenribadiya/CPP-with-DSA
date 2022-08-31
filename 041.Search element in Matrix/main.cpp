889216aa140131828042fd20cad936eed9dddff0
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-08-31 21:20:26 +0530

    Update main.cpp

--- 041.Search element in Matrix/main.cpp
+++ 041.Search element in Matrix/main.cpp
@@ -1,1 +1,50 @@
+ #include<iostream>
+ using namespace std;
 
+ int main()
+ {
+    int n,m;
+    cin>>n>>m;
+    int a[n][m];
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            cin>>a[i][j];
+        }
+    }
+
+    cout<<"Matrix is:"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    int num;
+    cin>>num;
+    bool flag=0;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            if(a[i][j]==num)
+            {
+                cout<<"Element found on following indices:"<<endl;
+                cout<<i<<" "<<j<<endl;
+                flag=1;
+                break;
+            }
+        }
+    }
+
+    if(flag==0)
+    {
+        cout<<"element does not exist!!!"<<endl;
+    }
+    return 0;
+ }
