8bb251e98c9a37f3aa051f7644ea4cdf9841d4cd
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-09-02 20:27:01 +0530

    comments added

--- 043. Matrix Transpose/main.cpp
+++ 043. Matrix Transpose/main.cpp
@@ -1,1 +1,36 @@
+// program to find transpose of a matrix.
 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    // printing initial logic.
+    cout<<"Matrix is:"<<endl;
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {cout<<a[i][j]<<" ";}
+        cout<<endl;
+    }
+
+    for(int i=0;i<3;i++) // swaping logic.
+    {
+        for(int j=i;j<3;j++)
+        {
+            int temp=a[i][j];
+            a[i][j]=a[j][i];
+            a[j][i]=temp;
+        }
+    }
+
+    cout<<"transpose is:"<<endl;
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {cout<<a[i][j]<<" ";}
+        cout<<endl;
+    }
+    return 0;
+}
