8603a6f6336bdb8797368453f7b4773f95712501
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-09-04 20:02:13 +0530

    code added

--- 046.Character Arrays/main.cpp
+++ 046.Character Arrays/main.cpp
@@ -1,1 +1,16 @@
+// Basic program regarding character array.
 
+// last charater will be null character. e.g "\0" 
+#include<iostream>
+using namespace std;
+int main()
+{
+    char c[100]="apple";
+    cout<<c<<endl;
+
+    char a[100];
+    cin>>a;
+    cout<<a<<endl;
+
+    return 0;
+}
