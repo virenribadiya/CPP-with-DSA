4356e0e0986c6cdc80d395857419e69ff2294e4c
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-09-05 22:08:51 +0530

    code added

--- 047.Palindrome/palindrome.cpp
+++ 047.Palindrome/palindrome.cpp
@@ -1,1 +1,33 @@
+// time complexity = 0(n) 
 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    char c[n];
+    cin>>c;
+    int f=0,l=n-1;
+    bool flag=true;
+    while(f!=n)
+    {
+        if(c[f]==c[l])
+        {
+            f++;
+            l--;
+        }
+        else
+        {
+            cout<<"not a palindrome!!"<<endl;
+            flag=false;
+            break;
+        }
+    }
+    if(flag==true)
+    {
+        cout<<"Palindrome!!!"<<endl;
+    }
+    return 0;
+}
