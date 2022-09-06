2dfb6873b2ba7c7a7756d1dcdd4d9e971fa9c79e
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-09-06 21:22:28 +0530

    pointers code added

--- 050.Pointers/pointers.cpp
+++ 050.Pointers/pointers.cpp
@@ -1,1 +1,15 @@
+// Pointers are variables that stores the address of the other variables.
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=10;
+    int *aptr=&a; // that means int *aptr; aptr=&a;
+    // we are using above format to reduce the lenght of code.
+    cout<<aptr<<endl; // gives the address of a.
+    cout<<*aptr<<endl; // gives the value of a.
+    return 0;
+}
 
