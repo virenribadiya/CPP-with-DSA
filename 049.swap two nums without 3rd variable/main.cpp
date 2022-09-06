e08643611a449f22e50bffaf7eade3e72ff2b1bd
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-09-06 21:15:26 +0530

    code added

--- 049.swap two nums without 3rd variable/main.cpp
+++ 049.swap two nums without 3rd variable/main.cpp
@@ -1,1 +1,16 @@
+// swap two numbers without third variable.
 
+#include <iostream>  
+using namespace std;  
+int main()  
+{  
+    //int a=5, b=10;
+    int a,b;
+    cin>>a>>b;      
+    cout<<"Before swap a= "<<a<<" b= "<<b<<endl;      
+    a=a*b; //a=50 (5*10)    
+    b=a/b; //b=5 (50/10)    
+    a=a/b; //a=10 (50/5)    
+    cout<<"After swap a= "<<a<<" b= "<<b<<endl;      
+    return 0;  
+} 
