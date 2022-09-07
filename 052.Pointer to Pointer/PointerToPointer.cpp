9dd828698baf6aa9894271484d42fb199820516c
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-09-07 16:19:22 +0530

    code added
    concept of double pointer is introduce.

--- 052.Pointer to Pointer/PointerToPointer.cpp
+++ 052.Pointer to Pointer/PointerToPointer.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+    //let the address of a is 2000.
+    //let the address of p is 4000.
+    int a=10;
+    int *aptr=&a;
+    cout<<aptr<<endl;
+    int **q=&aptr;
+    cout<<*q<<endl;//2000 -- address of a.
+    cout<<**q<<endl;//4000 -- address of p.
+    return 0;
+}
