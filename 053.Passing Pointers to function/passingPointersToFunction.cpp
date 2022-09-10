920f88e273540ca60cfdfc005c7a6b168acc4eaf
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-09-10 21:49:17 +0530

    Update passingPointersToFunction.cpp

--- 053.Passing Pointers to function/passingPointersToFunction.cpp
+++ 053.Passing Pointers to function/passingPointersToFunction.cpp
@@ -8,6 +8,7 @@ void swap(int *a,int *b)
     int temp = *a;
     *a=*b;
     *b=temp;
+    return;
 }
 
 int main()
@@ -15,7 +16,7 @@ int main()
     int a=10,b=20;
     int *aptr=&a,*bptr=&b; // storing address of a inside the pointer variables.
 
-    swap(aptr,bptr);
+    swap(aptr,bptr); // function 
     cout<<a<<" "<<b<<endl;
     return 0;
 }
