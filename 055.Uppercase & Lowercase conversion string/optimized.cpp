45c8b3de3168e1ae1b794360be36582699145d76
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-09-11 20:42:02 +0530

    file of optimized code added

---
+++ 055.Uppercase & Lowercase conversion string/optimized.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    string str = "FBdghjgbsdnkjsaa";
+    transform(str.begin(),str.end(),str.begin(),::toupper);
+    cout<<str<<endl;
+    transform(str.begin(),str.end(),str.begin(),::tolower);
+    cout<<str<<endl;
+    return 0;
+}
