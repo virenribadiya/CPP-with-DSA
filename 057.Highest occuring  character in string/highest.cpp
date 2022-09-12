3fd9e8fb8ef1ba1c7dd6dedb285e9857ae9adb41
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-09-12 17:10:33 +0530

    increase code readability

--- 057.Highest occuring  character in string/highest.cpp
+++ 057.Highest occuring  character in string/highest.cpp
@@ -7,8 +7,10 @@ using namespace std;
 int main()
 {
     string str = "aabbbccccdddddddfffffffffffff";
+    // array to store frequency of every individual alphabets.
     int freq[26];
-    for(int i=0;i<26;i++)
+    
+    for(int i=0;i<26;i++) 
     {
         freq[i]=0;
     }
