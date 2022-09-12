d233e5e7c66e15958a2b11f769b7786e6c409ca1
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-09-12 17:12:45 +0530

    basics of bit Manupulation

---
+++ 058.Bit Manupulation (getBit, setBit, clearBit)/BitManupulation.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int getBit(int num, int pos)
+{
+    return (num & (1<<pos) != 0);
+}
+
+int setBit(int num, int pos)
+{
+    return (num | (1<<pos));
+}
+
+int clearBit(int num, int pos)
+{
+    return (num & (~(1<<pos)));
+}
+
+int updateBit(int num, int pos, int value)
+{
+    int clear=clearBit(num,pos);
+    return (clear | (value<<pos));
+}
+
+int main()
+{
+    // cout<<getBit(5,2)<<endl;
+    // cout<<setBit(5,1)<<endl;
+    // cout<<clearBit(5,2)<<endl;
+    cout<<updateBit(5,1,1)<<endl;
+    return 0;
+}
