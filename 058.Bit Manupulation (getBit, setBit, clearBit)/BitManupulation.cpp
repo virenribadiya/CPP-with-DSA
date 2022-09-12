0976f43e3bf28155066b0934c94d6f4c85277420
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-09-12 17:16:54 +0530

    Update BitManupulation.cpp

--- 058.Bit Manupulation (getBit, setBit, clearBit)/BitManupulation.cpp
+++ 058.Bit Manupulation (getBit, setBit, clearBit)/BitManupulation.cpp
@@ -24,9 +24,9 @@ int updateBit(int num, int pos, int value)
 
 int main()
 {
-    // cout<<getBit(5,2)<<endl;
-    // cout<<setBit(5,1)<<endl;
-    // cout<<clearBit(5,2)<<endl;
+    cout<<getBit(5,2)<<endl;
+    cout<<setBit(5,1)<<endl;
+    cout<<clearBit(5,2)<<endl;
     cout<<updateBit(5,1,1)<<endl;
     return 0;
 }
