e8397978c70b741bd14b20be0e00d2e20cf9b78a
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-09-12 17:05:33 +0530

    highest occurring character code added

---
+++ 057.Highest occuring  character in string/highest.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+    string str = "aabbbccccdddddddfffffffffffff";
+    int freq[26];
+    for(int i=0;i<26;i++)
+    {
+        freq[i]=0;
+    }
+
+
+    for(int i=0;i<str.length();i++)
+    {
+        freq[str[i]-'a']++;
+    }
+
+    char ans='a';
+    int maxF = 0;
+
+    for(int i=0;i<26;i++)
+    {
+        if(freq[i]>maxF)
+        {
+            maxF=freq[i];
+            ans = i+'a';
+        }
+    }
+    cout<<maxF<<endl;
+    cout<<ans<<endl;
+
+    return 0;
+}
