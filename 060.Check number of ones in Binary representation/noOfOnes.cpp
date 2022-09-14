115c7f4d52e4275da1163c4f3a3c2e94cd18221f
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-09-14 19:17:16 +0530

    code added

--- 060.Check number of ones in Binary representation/noOfOnes.cpp
+++ 060.Check number of ones in Binary representation/noOfOnes.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+using namespace std;
 
+int numOfOnes(int n)
+{
+    int count=0;
+    while(n!=0) // same as while(n)
+    {
+        n=n&(n-1);
+        count++;
+    }
+    return count;
+}
+
+int main()
+{
+    cout<<numOfOnes(19)<<endl;
+}
