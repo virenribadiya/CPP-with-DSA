861cdc627b82e2b3807d0d908c042c7057583311
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-09-15 19:01:00 +0530

    code added

--- 061.Subsets of set/subsets.cpp
+++ 061.Subsets of set/subsets.cpp
@@ -1,1 +1,26 @@
 
+#include<iostream>
+using namespace std;
+
+void subset(char a[],int n)
+{
+    for(int i=0;i<(1<<n);i++) //here 1<<n=8
+    {
+        for(int j=0;j<n;j++)
+        {
+            if((i & (1<<j))!=0)
+            {
+                cout<<a[j]<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return;
+}
+
+int main()
+{
+    char a[]={'a','b','c'};
+    subset(a,3);
+    return 0;
+}
