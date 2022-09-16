e01d4812c0608de25ac719c55b8d66ba075f8ec5
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-09-16 18:38:04 +0530

    code added

--- 062. Unique in an Array/unique.cpp
+++ 062. Unique in an Array/unique.cpp
@@ -1,1 +1,21 @@
 
+#include<iostream>
+using namespace std;
+
+int unique(int a[],int n)
+{
+    int xorsum=0;
+    for(int i=0;i<n;i++)
+    {
+        xorsum=xorsum^a[i];
+        cout<<xorsum<<endl;
+    }
+    return xorsum;
+}
+
+int main()
+{
+    int a[]={1,2,3,4,1,2,3};
+    cout<<unique(a,7);
+    return 0;
+}
