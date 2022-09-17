93db7f7fc66e3bcb91c25c14e3fbc3f444e16914
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-09-17 10:10:16 +0530

    code added

--- 064.Inclusion-Exclusion Principle/inEx.cpp
+++ 064.Inclusion-Exclusion Principle/inEx.cpp
@@ -1,1 +1,24 @@
 
+// Program to find total numbers from to 1 to n that are divisible by a OR b.
+// ex: find total numbers from 1 to 40 that are divisible by 5 OR 7.
+
+#include<iostream>
+using namespace std;
+
+int divisible(int n, int a, int b)
+{
+    int c1=n/a; // total numbers that are divisible by a.
+    int c2=n/b; // total numbers that are divisible by b.
+    int c3=n/(a*b); // total numbers that are divisible by a and b both.
+
+    return c1+c2-c3; // inclusion - exclusion principle.
+}
+
+int main()
+{
+    int n,a,b;
+    cin>>n>>a>>b;
+    cout<<divisible(n,a,b)<<endl;
+
+    return 0;
+}
