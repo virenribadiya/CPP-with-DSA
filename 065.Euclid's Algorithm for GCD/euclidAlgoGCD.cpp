39c2cf0deda7739d3a62b1a9e06aba29eaef4482
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-09-18 08:52:29 +0530

    code of Euclid algo added

--- 065.Euclid's Algorithm for GCD/euclidAlgoGCD.cpp
+++ 065.Euclid's Algorithm for GCD/euclidAlgoGCD.cpp
@@ -1,1 +1,24 @@
 
+// Euclid's Algorithm to find GCD.
+
+#include<iostream>
+using namespace std;
+
+int gcd(int a, int b)
+{
+    int rem;
+    while((rem=a%b)!=0)
+    {
+        a=b;
+        b=rem;
+    }
+    return b;
+}
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<gcd(a,b)<<endl;
+    return 0;
+}
