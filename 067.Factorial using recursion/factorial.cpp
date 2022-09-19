32baa7ac3465fe4eba1979f5f909d66a812a120f
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-09-19 10:11:38 +0530

    code added

--- 067.Factorial using recursion/factorial.cpp
+++ 067.Factorial using recursion/factorial.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+using namespace std;
 
+int factorial(int n)
+{
+    if(n==1)
+        return 1;
+    int prevAns = factorial(n-1);
+    return n*prevAns;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<factorial(n)<<endl;
+    return 0;
+}
