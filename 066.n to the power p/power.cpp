ae2fac9bdb60b09b1f3dd8b78351ef27c4291f73
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-09-19 10:00:41 +0530

    code added

--- 066.n to the power p/power.cpp
+++ 066.n to the power p/power.cpp
@@ -1,1 +1,22 @@
+// calculate n to the power p using recursion.
 
+#include<iostream>
+using namespace std;
+
+int power(int n, int p)
+{
+    if(p==0)
+    {
+        return 1;
+    }
+    int prevPowerAns=power(n,p-1);
+    return n*prevPowerAns;
+}
+
+int main()
+{
+    int n,p;
+    cin>>n>>p;
+    cout<<power(n,p)<<endl;
+    return 0;
+}
