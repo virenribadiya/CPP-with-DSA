f324e1db491714e363b013487c52c872280ccc42
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-09-20 20:00:46 +0530

    code added

--- 068.nth Fibonacci number/Fibonacci.cpp
+++ 068.nth Fibonacci number/Fibonacci.cpp
@@ -1,1 +1,21 @@
+// Explaination: Fib(position of number)
+// Fib(n)=Fib(n-1)+Fib(n-2)
+// Base Condition: Fib(0)=0,Fib(1)=1
 
+#include<iostream>
+using namespace std;
+
+int Fibonacci(int n)
+{
+    if(n==0 || n==1)
+        return n;
+    return Fibonacci(n-1)+Fibonacci(n-2);
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<Fibonacci(n)<<endl;
+    return 0;
+}
