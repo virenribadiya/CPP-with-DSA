5288720135ca2f242b0beae5e1418d18fb42eb96
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-09-21 21:43:01 +0530

    Create sortedCheck.cpp

--- 069.Check if an array is sorted or not/sortedCheck.cpp
+++ 069.Check if an array is sorted or not/sortedCheck.cpp
@@ -1,1 +1,16 @@
+#include<iostream>
+using namespace std;
 
+bool sorted(int a[], int n)
+{
+    if(n==1)
+        return true;
+    return(a[0]<a[1] && sorted(a+1,n-1));
+}
+
+int main()
+{
+    int a[]={0,2,3,4,5};
+    cout<<sorted(a,5)<<endl;
+    return 0;
+}
