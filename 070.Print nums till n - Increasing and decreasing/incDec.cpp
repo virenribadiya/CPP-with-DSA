fa9e89321b44912126e2990dd2b04b94715425bb
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-09-22 21:33:36 +0530

    increasing code added

--- 070.Print nums till n - Increasing and decreasing/incDec.cpp
+++ 070.Print nums till n - Increasing and decreasing/incDec.cpp
@@ -1,1 +1,24 @@
 
+#include<iostream>
+using namespace std;
+
+void inc(int n)
+{
+    if(n==1)
+    {
+        cout<<n<<endl;
+        return;
+    }
+    inc(n-1);
+    cout<<n<<endl;
+    return;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    inc(n);
+    //dec(n);
+    return 0;
+}
