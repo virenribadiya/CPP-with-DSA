1a40d4061da66f711c6bf891f41365636f57c23f
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-09-23 21:46:00 +0530

    last occurance code added

--- 071.First occurance-Last occurance in Array/occurance.cpp
+++ 071.First occurance-Last occurance in Array/occurance.cpp
@@ -15,11 +15,24 @@ int foccur(int a[],int n,int i,int key)
     return foccur(a,n,i+1,key);
 }
 
+int loccur(int a[],int i,int key)
+{
+    if(i==-1)
+    {
+        return -1;
+    }
+    if(a[i]==key)
+    {
+        return i;
+    }
+    return loccur(a,i-1,key);
+}
+
 
 int main()
 {
     int a[]={1,2,3,4,5,2,3};
     cout<<foccur(a,7,0,2)<<endl;
-    //cout<<loccur(a,6,2)<<endl;
+    cout<<loccur(a,6,2)<<endl;
     return 0;
 }
