b043aebae9e34f9fdbf7922874b35b9209571a59
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-09-24 22:50:04 +0530

    code added

--- 072.Reverse String using Recursion/reverseStr.cpp
+++ 072.Reverse String using Recursion/reverseStr.cpp
@@ -1,1 +1,23 @@
 
+#include<iostream>
+using namespace std;
+
+void reverseStr(string str)
+{
+    if(str.length()==0)
+    {
+        return;
+    }
+    string rest=str.substr(1);
+    //cout<<"str="<<str<<endl;
+    //cout<<"rest="<<rest<<endl;
+    reverseStr(rest);
+    cout<<str[0];
+    return;
+}
+
+int main()
+{
+    reverseStr("Vinod");
+    return 0;
+}
