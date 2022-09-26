4356395ae656a6ab979c2d08996e75d482dce70c
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-09-26 19:35:11 +0530

    solution code added

--- 074.Tower of Hanoi/ToH.cpp
+++ 074.Tower of Hanoi/ToH.cpp
@@ -1,1 +1,23 @@
 
+
+#include<iostream>
+using namespace std;
+
+void towerofHanoi(int n,char source,char helper,char dest)
+{
+    if(n==1)
+    {
+        cout<<"Move from "<<source<<" to "<<dest<<endl;
+        return;
+    }
+    towerofHanoi(n-1,source,dest,helper);
+    cout<<"Move from "<<source<<" to "<<dest<<endl;
+    towerofHanoi(n-1,helper,source,dest);
+    return;
+}
+
+int main()
+{
+    towerofHanoi(3,'A','B','C');
+    return 0;
+} 
