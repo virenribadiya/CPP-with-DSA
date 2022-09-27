068de603e1c07e107c3bd4e914e7e3810136ae7c
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-09-27 20:08:24 +0530

    solution code added

--- 075.Move all x to end in a String/Movex.cpp
+++ 075.Move all x to end in a String/Movex.cpp
@@ -1,1 +1,23 @@
 
+#include<iostream>
+using namespace std;
+
+string moveToEnd(string s)
+{
+    if(s.length()==0)
+    {
+        return "";
+    }
+    string ans = moveToEnd(s.substr(1));
+    if(s[0]=='x')
+    {
+        return ans+s[0];
+    }
+    return s[0]+ans;
+}
+
+int main()
+{
+    cout<<moveToEnd("xasddfxrfwgxxyx")<<endl;
+    return 0;
+}
