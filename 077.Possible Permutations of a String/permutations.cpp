298f8d444bf67ef0d17e0ef6151721e1dd4f056b
Viren Ribadiya <108461765+virenribadiya@users.noreply.github.com>
2022-09-29 22:13:39 +0530

    solution code added

--- 077.Possible Permutations of a String/permutations.cpp
+++ 077.Possible Permutations of a String/permutations.cpp
@@ -1,1 +1,28 @@
 
+// print all the possible permuations of a string using recursion.
+
+#include<iostream>
+using namespace std;
+
+void permutations(string s, string ans)
+{
+    if(s.length()==0)
+    {
+        cout<<ans<<endl;
+        return;
+    }
+
+    for(int i=0;i<s.length();i++)
+    {
+        char ch = s[i];
+        string ros = s.substr(0,i)+s.substr(i+1);
+        permutations(ros,ans+ch);
+    }
+    return;
+}
+
+int main()
+{
+    permutations("ABC","");
+    return 0;
+}
