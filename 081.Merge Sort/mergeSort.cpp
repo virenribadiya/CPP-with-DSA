774ca468e92a1037a81369eace908418a7f9765e
Viren Ribadiya <vribadiya278@rku.ac.in>
2022-10-07 23:13:39 +0530

    merge fn code added

--- 081.Merge Sort/mergeSort.cpp
+++ 081.Merge Sort/mergeSort.cpp
@@ -2,6 +2,53 @@
 #include<iostream>
 using namespace std;
 
+void merge(int arr[],int f,int mid,int l)
+{
+       int n1 = mid-f+1;
+       int n2 = l-mid;
+
+       int a[n1];
+       int b[n2];
+
+       for(int i=0;i<n1;i++)
+       {
+            a[i]=arr[f+i];
+       }
+
+       for(int i=0;i<n2;i++)
+       {
+            b[i]=arr[mid+1+i];
+       }
+
+       int i=0,j=0,k=f;
+
+       while(i<n1 && j<n2)
+       {
+            if(a[i]<b[j])
+            {
+                arr[k]=a[i];
+                k++;i++;
+            }
+            else
+            {
+                arr[k]=b[j];
+                k++;j++;
+            }
+       }
+
+       while(i<n1)
+       {
+            arr[k]=a[i];
+            k++;i++;
+       }
+
+       while(j<n2)
+       {
+            arr[k]=b[j];
+            k++;j++;
+       }
+}
+
 void mergeSort(int arr[],int f,int l)
 {
     if(f<l)
