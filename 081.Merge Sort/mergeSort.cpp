76a6d3ca59e1a196f07b8e2062d50da6288ba30c
Viren Ribadiya <vribadiya278@rku.ac.in>
2022-10-07 23:11:45 +0530

    code of main fn added

--- 081.Merge Sort/mergeSort.cpp
+++ 081.Merge Sort/mergeSort.cpp
@@ -1,1 +1,19 @@
 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a[5]={4,9,2,4,1};
+    for(int i=0;i<5;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+    mergeSort(a,0,4);
+    for(int i=0;i<5;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
