4d604cc9834ef356b1c0fc73373357a0e0e22313
Viren Ribadiya <vribadiya278@rku.ac.in>
2022-10-08 20:33:37 +0530

    user preference added

--- 081.Merge Sort/mergeSort.cpp
+++ 081.Merge Sort/mergeSort.cpp
@@ -62,7 +62,14 @@ void mergeSort(int arr[],int f,int l)
 
 int main()
 {
-    int a[5]={4,9,2,4,1};
+    //int a[5]={4,9,2,4,1};
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
     for(int i=0;i<5;i++)
     {
         cout<<a[i]<<" ";
