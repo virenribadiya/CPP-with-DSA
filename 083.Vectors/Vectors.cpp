c7c3b71ed7ef20f666300ea1546d22ca99acb4f3
Viren Ribadiya <vribadiya278@rku.ac.in>
2022-10-15 21:20:45 +0530

    Comments added

--- 083.Vectors/Vectors.cpp
+++ 083.Vectors/Vectors.cpp
@@ -8,8 +8,8 @@ using namespace std;
  
 int main()
 {
-    vector<int> g1;
- 
+    vector<int> g1; //initialisation of vector
+    // Inbuilt methods
     for (int i = 1; i <= 5; i++)
         g1.push_back(i);
  
