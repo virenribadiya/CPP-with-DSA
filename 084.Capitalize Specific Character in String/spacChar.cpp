bfdd3d36c4f72bb5c3c54ac67e6e2b822973c274
Viren Ribadiya <vribadiya278@rku.ac.in>
2022-10-18 22:42:25 +0530

    Code added

--- 084.Capitalize Specific Character in String/spacChar.cpp
+++ 084.Capitalize Specific Character in String/spacChar.cpp
@@ -1,1 +1,34 @@
 
+#include<iostream>
+#include<string.h>
+#include<stdio.h>
+
+using namespace std;
+int main()
+{
+   char str[200], ch, ch_temp;
+   int len, i, asc_val;
+   cout<<"Enter the String: ";
+   gets(str);
+   cout<<"Enter the Character to Capitalize: ";
+   cin>>ch;
+   len = strlen(str);
+   for(i=0; i<len; i++)
+   {
+      if(ch==str[i])
+      {
+         asc_val = str[i];
+         if(asc_val>=97 && asc_val<=122)
+         {
+            asc_val = asc_val-32;
+            ch_temp = asc_val;
+            str[i] = ch_temp;
+         }
+      }
+   }
+   cout<<"\nAll \""<<ch<<"\" gets capitalized";
+   cout<<"\nThe new string is\n\n";
+   cout<<str;
+   cout<<endl;
+   return 0;
+}
