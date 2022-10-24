e65817f0f5d0f0a66614f9b490488109e99573db
Viren Ribadiya <vribadiya278@rku.ac.in>
2022-10-24 22:23:16 +0530

    Code added

--- 087.Without modulas Even or Odd/noMode.cpp
+++ 087.Without modulas Even or Odd/noMode.cpp
@@ -1,1 +1,23 @@
 
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+	int n; 
+	
+	cout<<"Enter Number: ";
+	cin>>n;
+
+	while(n>1)
+	{
+		n = n-2;
+	}
+
+	if(n==0)
+		cout<<"Even Number"<<endl;
+	else
+		cout<<"Odd Number"<<endl;
+
+	return 0;
+}
