e23386df08e349bf1de154ceed57ed3eedeaa02d
Viren Ribadiya <vribadiya278@rku.ac.in>
2022-10-28 20:10:55 +0530

    Update concate.cpp

--- 088.Concatenate Two Strings using strcat/concate.cpp
+++ 088.Concatenate Two Strings using strcat/concate.cpp
@@ -8,9 +8,9 @@ int main()
         char str[50],str1[50];
 
         cout<<"\nEnter First String :: ";
-        gets(str);
+        gets(str); // to take input from the user.
         cout<<"\nEnter Second String :: ";
-        gets(str1);
+        gets(str1); // to take input from the user.
 
         strcat(str,str1); // inbuilt function having two arguments.
 
