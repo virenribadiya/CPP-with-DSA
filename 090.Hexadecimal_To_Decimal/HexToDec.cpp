ace21e5ad2aa3584b356b8a14d612164f6c72bb7
Viren Ribadiya <vribadiya278@rku.ac.in>
2022-10-31 21:22:27 +0530

    code added

--- 090.Hexadecimal_To_Decimal/HexToDec.cpp
+++ 090.Hexadecimal_To_Decimal/HexToDec.cpp
@@ -1,1 +1,34 @@
 
+#include<iostream>
+using namespace std;
+
+int hexToDecimal(string n)
+{
+    int ans=0;
+    int poweranswer=1;
+
+    int s=n.size();
+    for(int i=s-1;i>=0;i--)
+    {
+        if(n[i]>='0' && n[i]<='9')
+        {
+            ans=ans+poweranswer*(n[i]-'0');
+        }
+        else if(n[i]>='A' && n[i]<='F')
+        {
+            ans=ans+poweranswer*(n[i]-'A'+10);
+        }
+        poweranswer=poweranswer*16;
+    }
+    return ans;
+}
+
+
+int main()
+{
+    string n;
+    cin>>n;
+
+    cout<<hexToDecimal(n)<<endl;
+    return 0;
+}
